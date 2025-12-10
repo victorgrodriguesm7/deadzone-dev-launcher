0b163b13470f66cb5ee3f884d4ed2bc3aba5b2b8
Victor <victorgrodriguesm7@gmail.com>
2025-12-10 10:08:55 -0300

    feat: add local assets env variables

--- src/env.js
+++ src/env.js
@@ -1,3 +1,5 @@
 module.exports = {
-    baseUrl: process.env["API_BASE_URL"]
+    baseUrl: process.env["API_BASE_URL"],
+    localAssets:  process.env["LOCAL_ASSETS_URL"],
+    localAssetsPort:  process.env["LOCAL_ASSETS_PORT"]
 }
