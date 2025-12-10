cd42b348d2e9f8d934579117193e8805ca357a57
Victor <victorgrodriguesm7@gmail.com>
2025-12-10 10:39:52 -0300

    feat: changed build process

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
-
 const path = require("path");
+
+require('dotenv').config({ path: __dirname + "/.env" })
+
 const { app, session, BrowserWindow  } = require('electron');
 const { addFlashPlayerLib } = require("./src/flashlib");
 const { startServer } = require("./src/server");
